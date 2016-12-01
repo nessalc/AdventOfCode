7c59a681c476a3db3bf106b6f205dbc6b15e3fb9
Nessalc <jclassen@gmail.com>
2016-12-01 09:30:18 -0600

    Part 2

--- advent2016/day01.rb
+++ advent2016/day01.rb
@@ -1,8 +1,8 @@
-f=File.open("C:\\Users\\James\\Documents\\programming\\advent2016\\input01.txt")
+f=File.open("C:\\Users\\c41663\\Documents\\programming\\githome\\AdventOfCode\\advent2016\\input01.txt")
 directions=f.read
 compass='NESW'
 x,y,f=0,0,'N'
-locations=[]
+locations=[[x,y]]
 directions.split(', ').each do |d|
   lr,dist=d[/[LR]/],d[/\d+/].to_i
   if lr=='L' then
@@ -10,15 +10,42 @@ directions.split(', ').each do |d|
   else
     f=compass[(compass.index(f)+1)%4]
   end
+  xp,yp=x,y
+  temp=[]
   if f=='N' then
     y+=dist
+    ((yp+1)..y).each do |j|
+      temp+=[[x,j]]
+    end
   elsif f=='E' then
     x+=dist
+    ((xp+1)..x).each do |i|
+      temp+=[[i,y]]
+    end
   elsif f=='S' then
     y-=dist
+    (y...yp).each do |j|
+      temp+=[[x,j]]
+    end
+    temp.reverse!
   elsif f=='W' then
     x-=dist
+    (x...xp).each do |i|
+      temp+=[[i,y]]
+    end
+    temp.reverse!
   end
-  locations+=[[x,y]]
+  locations+=temp
 end
-puts x.to_s+'+'+y.to_s+'='+(x+y).to_s
+puts "part 1: #{x.abs}+#{y.abs}=#{x.abs+y.abs}"
+duplicate_found=false
+for i in 1...locations.length
+  if !duplicate_found
+    index=locations[0,i].index(locations[i])
+    if index
+      x,y=locations[i]
+      duplicate_found=true
+    end
+  end
+end
+puts "part 2: #{x.abs}+#{y.abs}=#{x.abs+y.abs}"
