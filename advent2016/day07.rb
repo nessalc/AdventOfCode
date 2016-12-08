c99b2ab090b71c51601cbf6b86465e12005d4cc9
Nessalc <jclassen@gmail.com>
2016-12-08 11:06:31 -0600

    Day 7 Part 2
    Finally!

--- advent2016/day07.rb
+++ advent2016/day07.rb
@@ -28,14 +28,36 @@ lines.partition.each do |line|
     count+=1
   end
 end
+puts "Part 1: #{count} Addresses support TLS"
+count=0
 lines.partition.each do |line|
   #supernet sequences *outside* brackets
   #hypernet sequences *inside* brackets
-  hypernet=line.scan(/(?<=\[)\w*/)
-  supernet=line.scan(/\w*(?=\[)|(?<=\])\w*/)
+  hypernet=line.scan(/(?<=\[)\w+/)
+  supernet=line.scan(/\w+(?=\[)|(?<=\])\w+/)
   #supports SSL
     #contains aba *outside* square brackets
     #contains bab *inside* square brackets
-  aba=false
+  ssl=false
+  supernet.each do |segment|
+    pos=segment.index(/(\w)(\w)\1/)
+    while pos
+      val=segment.match(/(\w)(\w)\1/,pos)
+      if val and val[1]!=val[2]
+        a,b=val[1,2]
+        hypernet.each do |hseg|
+          ssl|=hseg[b+a+b]
+        end
+        if ssl
+          break
+        end
+      end
+      pos=segment.index(/(\w)(\w)\1/,pos+1)
+    end
+  end
+  if ssl
+    count+=1
+    next
+  end
 end
-puts "Part 1: #{count} Addresses support TLS"
+puts "Part 2: #{count} Addresses support SSL"
