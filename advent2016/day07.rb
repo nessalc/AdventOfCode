d1d7cf32794e21b21fff037c0fefa9b91cd3413b
Nessalc <jclassen@gmail.com>
2016-12-07 21:26:20 -0600

    Day 7 Part 1

--- advent2016/day07.rb
+++ advent2016/day07.rb
@@ -1,18 +1,41 @@
 f=File.open(ARGV[0])
 lines=f.readlines()
 count=0
-lines.each do |line|
-  line.scan(/\[[^\]]*?(\w)(\w)\2\1[^\[]*?\]/).each do |test1|
-    unless test1[0]!=test1[1]
-      line.scan(/(\w)(\w)\2\1/).each do |test2|
-        if test2[0]!=test2[1]
-          count+=1
-	  break
-        end
-      end
-    else
-      break
+lines.partition.each do |line|
+  #supernet sequences *outside* brackets
+  #hypernet sequences *inside* brackets
+  hypernet=line.scan(/(?<=\[)\w*/)
+  supernet=line.scan(/\w*(?=\[)|(?<=\])\w*/)
+  #supports TLS
+    #contains abba *outside* square brackets
+  abba=false
+  hypernet.each do |test|
+    t=test.match(/(\w)(\w)\2\1/)
+    if t
+      abba|=(t[1]!=t[2])
     end
   end
+  if abba
+    next
+  end
+  supernet.each do |test|
+    t=test.match(/(\w)(\w)\2\1/)
+    if t
+      abba|=(t[1]!=t[2])
+    end
+  end
+  if abba
+    count+=1
+  end
+end
+lines.partition.each do |line|
+  #supernet sequences *outside* brackets
+  #hypernet sequences *inside* brackets
+  hypernet=line.scan(/(?<=\[)\w*/)
+  supernet=line.scan(/\w*(?=\[)|(?<=\])\w*/)
+  #supports SSL
+    #contains aba *outside* square brackets
+    #contains bab *inside* square brackets
+  aba=false
 end
 puts "Part 1: #{count} Addresses support TLS"
