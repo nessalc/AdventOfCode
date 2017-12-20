ab53d0901b3f62d97c36e5829ecb0a9fff8c2a26
Nessalc <jclassen@gmail.com>
2017-12-20 15:36:19 -0600

    Day 13 complete!

--- advent2017/day13.py
+++ advent2017/day13.py
@@ -6,10 +6,11 @@ class Scanner:
     direction=1
     def __init__(self,depth):
         self.depth=depth
-    def update(self):
-        self.position+=self.direction
-        if self.position==self.depth-1 or self.position==0:
-            self.direction*=-1
+    def update(self,steps=1):
+        for i in range(steps%((self.depth-1)*2)):
+            self.position+=self.direction
+            if self.position==self.depth-1 or self.position==0:
+                self.direction*=-1
     def __str__(self):
         return '<Scanner: {}{}>'.format(self.position,'+' if self.direction>0 else '-')
     def __repr__(self):
@@ -22,22 +23,23 @@ class Firewall:
         for line in layer_desc:
             a,b=tuple(map(int,line.strip().split(':')))
             self.layer[a]=Scanner(b)
-    def update_scanners(self):
+    def update_scanners(self,steps=1):
         for s in self.layer.keys():
-            self.layer[s].update()
+            self.layer[s].update(steps)
     def move_packet(self):
         self.packetpos+=1
     def is_caught(self):
         return (self.packetpos in self.layer.keys() and self.layer[self.packetpos].position==0)
     def calculate_severity(self,delay,stop_once_caught=False):
         severity=caught_counter=0
-        self.packetpos=-delay-1
-        for i in range(max(self.layer.keys())+delay+1):
+        self.update_scanners(delay)
+        for i in range(max(self.layer.keys())+1):
             self.move_packet()
             if self.packetpos>=0 and self.is_caught():
                 severity+=self.packetpos*self.layer[self.packetpos].depth
                 caught_counter+=1
                 if stop_once_caught:
+                    self.reset()
                     return severity,caught_counter
             self.update_scanners()
         self.reset()
@@ -57,9 +59,7 @@ if __name__=='__main__':
     while caught_count:
         i+=1
         severity,caught_count=firewall.calculate_severity(i,True)
-        if i%100==0 and i%1000!=0:
-            print('.',end='')
-        elif i%1000==0:
-            print('*',end='')
-    print()
+        #if i%1000==0:
+        #    print('.',end='')
+    #print()
     print('Part 2: Delay {} picoseconds for a safe trip'.format(i))
